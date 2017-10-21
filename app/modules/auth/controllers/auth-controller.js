a3552bf0ef3b47aa6628060d6f40cc638eb8a656
vitaliy <khotey.v@ya.ru>
2017-10-21 16:16:39 +0300

    refactored fetch user

--- app/modules/auth/controllers/auth-controller.js
+++ app/modules/auth/controllers/auth-controller.js
@@ -4,7 +4,7 @@ import { User } from '../../users';
 export default {
   async signUp(ctx) {
     const { _id } = await User.create(pick(ctx.request.body, User.createFields));
-    const user = await User.findOne({ _id }).select({ password: 0 });
+    const user = await User.findOne({ _id }).select({ password: 0, _id: 0 });
 
     ctx.body = { data: user.toJSON() };
   },
