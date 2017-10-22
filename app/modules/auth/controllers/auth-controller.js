604d0b42c1b6aa57009233a09d95aed18f70df9a
vitaliy <khotey.v@ya.ru>
2017-10-22 11:58:46 +0300

    refactored user models

--- app/modules/auth/controllers/auth-controller.js
+++ app/modules/auth/controllers/auth-controller.js
@@ -4,7 +4,7 @@ import { User } from '../../users';
 export default {
   async signUp(ctx) {
     const { _id } = await User.create(pick(ctx.request.body, User.createFields));
-    const user = await User.findOne({ _id }).select({ password: 0, _id: 0, __v: 0 }).lean();
+    const user = await User.findOne({ _id }).select({ password: 0, _id: 0, __v: 0 });
 
     ctx.body = { data: user };
   },
