11fc004bbb22afd18d3bbee751a3fbf6af5c4f37
vitaliy <khotey.v@ya.ru>
2017-11-01 16:16:57 +0200

    fixed word

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ import { PORT } from './config';
 const server = app.listen(PORT, (err) => {
   if (err) console.error(err);
 
-  console.error(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
 
 export default server;
